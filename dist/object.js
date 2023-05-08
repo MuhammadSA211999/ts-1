9d2e71313b5d98513d2d1c29bdb13079d8539b2c
Muhammad SA <MuhammadSA211999>
2023-05-08 15:26:30 +0600

    object in ts

---
+++ dist/object.js
@@ -0,0 +1,8 @@
+"use strict";
+const user = {
+    name: 'Muhammad SA',
+    age: 24,
+    company: 'TARMAT INC',
+    isMarried: false
+};
+console.log(user);
---
+++ src/object.ts
@@ -0,0 +1,17 @@
+const user: {
+    name: string;
+    age: number;
+    company: string;
+    position: 'CEO';
+    //position hocce literel type
+    isMarried: boolean;
+    wife?: string
+} = {
+    name: 'Muhammad SA',
+    age: 24,
+    company: 'TARMAT INC',
+    position: 'CEO',
+    isMarried: false
+}
+
+console.log(user)
